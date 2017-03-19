c9fae89013bece378220b37b091d8afc4e3e35d7
shuming2 <Amber@ZhangShumingdeMacBook-Air.local>
2017-03-18 21:59:39 -0600

    done Atari Defense

--- Go4/board_util.py
+++ Go4/board_util.py
@@ -80,15 +80,17 @@ class GoBoardUtil(object):
             Use in UI only. For playing, use generate_move_with_filter
             which is more efficient
         """
-        
         if board.last_move != None:
-            ataricapture = GoBoardUtil.atari_capture(board, board.last_move, GoBoardUtil.opponent(board.current_player))
-            ataricapture = GoBoardUtil.filter_moves(board, ataricapture, check_selfatari)
-            if len(ataricapture) > 0:
-                return ataricapture, "AtariCapture"
-            
-        
-        
+            atari_capture_moves = GoBoardUtil.generate_atari_capture_moves(board)
+            atari_capture_moves = GoBoardUtil.filter_moves(board, atari_capture_moves, check_selfatari)
+            if len(atari_capture_moves) > 0:
+                return atari_capture_moves, "AtariCapture"
+
+            atari_defense_moves = GoBoardUtil.generate_atari_defense_moves(board)
+            atari_defense_moves = GoBoardUtil.filter_moves(board, atari_defense_moves, check_selfatari)
+            if len(atari_defense_moves) > 0:
+                return atari_defense_moves, "AtariDefense"
+
         pattern_moves = GoBoardUtil.generate_pattern_moves(board)
         pattern_moves = GoBoardUtil.filter_moves(board, pattern_moves, check_selfatari)
         if len(pattern_moves) > 0:
@@ -336,17 +338,62 @@ class GoBoardUtil(object):
             return 'pass'
         row, col = divmod(point, ns)
         return row,col
-        
-        
+
+    ### Assignment 3 - Atari Capture
     @staticmethod
-    def atari_capture(board, last_move, color):
-        if board._liberty(last_move, color) == 1:
-            fboard = board._flood_fill(board.last_move)
+    def generate_atari_capture_moves(board):
+        moves = []
+        move = GoBoardUtil.find_last_liberty(board, board.last_move)
+        if move:
+            moves.append(move)
+        return moves
+
+    ### Assignment 3 - Atari Defense
+    @staticmethod
+    def generate_atari_defense_moves(board):
+        moves = []
+        for nb in board._neighbors(board.last_move):
+            color = board.board[nb]
+            if color == board.current_player:
+                if board._liberty(nb, color) != 1:
+                    continue
+                # Run Away
+                move = GoBoardUtil.find_last_liberty(board, nb)
+                if move:
+                    moves.append(move)
+                # Capture
+                adjacent_opponent = GoBoardUtil.find_adjacent_opponent(board, nb)
+                adjacent_opponent.remove(board.last_move)
+                for point in adjacent_opponent:
+                    move = GoBoardUtil.find_last_liberty(board, point)
+                    if move:
+                        moves.append(move)
+        return list(set(moves))
+
+    ### Assignment 3 - Find the last liberty point
+    @staticmethod
+    def find_last_liberty(board, point):
+        if board._liberty(point, board._points_color(point)) == 1:
+            fboard = board._flood_fill(point)
             inds = list(*np.where(fboard == FLOODFILL))
             for f in inds:
                 f_neighbors = board._neighbors(f)
                 for n in f_neighbors:
                     if fboard[n]==EMPTY:
-                        return [int(n)]
+                        return n
         else:
-            return []
+            return None
+
+    ### Assignment 3 - Find the adjacent opponent points of the input point
+    @staticmethod
+    def find_adjacent_opponent(board, point):
+        adjacent_opponent = []
+        opponent_color = GoBoardUtil.opponent(board.board[point])
+        fboard = board._flood_fill(point)
+        inds = list(*np.where(fboard == FLOODFILL))
+        for f in inds:
+            f_neighbors = board._neighbors(f)
+            for n in f_neighbors:
+                if fboard[n] == opponent_color:
+                    adjacent_opponent.append(n)
+        return adjacent_opponent
